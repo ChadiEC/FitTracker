ebc53e45387e9f4b0f8042a30ca46d116c7087c4
ChadiEC <1985081@collegemv.qc.ca>
2025-02-06 14:50:40 -0500

    Création des forms

--- src/pages/Client.jsx
+++ src/pages/Client.jsx
@@ -2,9 +2,63 @@ import React from 'react';
 
 function Client() {
     return (
-        <div>
-            <h1>Ceci sera notre page d'inscription client</h1>
-        </div>
+        < div >
+        < form >
+            <h3>Incription en tant que client</h3><br/>
+            <div class="row mb-4">
+                <div class="col">
+                    <div data-mdb-input-init class="form-outline">
+                        <label class="form-label" for="form6Example1">Nom</label>
+                        <input type="text" id="form6Example1" class="form-control" />
+                    </div>
+                </div>
+                <div class="col">
+                    <div data-mdb-input-init class="form-outline">
+                        <label class="form-label" for="form6Example2">Prénom</label>
+                        <input type="text" id="form6Example2" class="form-control" />
+                    </div>
+                </div>
+            </div>
+
+
+            <div data-mdb-input-init class="form-outline mb-4">
+                <label class="form-label" for="form6Example3">Adresse couriel</label>
+                <input type="text" id="form6Example3" class="form-control" />
+
+            </div>
+
+
+            <div data-mdb-input-init class="form-outline mb-4">
+                <label class="form-label" for="form6Example4">Adresse </label>
+                <input type="text" id="form6Example4" class="form-control" />
+                
+            </div>
+
+
+            <div data-mdb-input-init class="form-outline mb-4">
+                <label class="form-label" for="form6Example5">Code postal</label>
+                <input type="email" id="form6Example5" class="form-control" />
+                
+            </div>
+
+
+            <div data-mdb-input-init class="form-outline mb-4">
+            <label class="form-label" for="form6Example6">Phone</label>
+                <input type="number" id="form6Example6" class="form-control" />
+                
+            </div>
+
+
+            <div data-mdb-input-init class="form-outline mb-4">
+            <label class="form-label" for="form6Example7">Information additionnel</label>
+                <textarea class="form-control" id="form6Example7" rows="4"></textarea>
+                
+            </div>
+
+
+            <button data-mdb-ripple-init type="button" class="btn btn-primary btn-block mb-4">Envoyer</button>
+        </form >
+    </div >
     );
 }
 
--- src/pages/Connexion.jsx
+++ src/pages/Connexion.jsx
@@ -2,9 +2,141 @@ import React from 'react';
 
 function Connexion() {
     return (
+
         <div>
-            <h1>Ici il y aura le formulaire de login pour les utilisateurs existant</h1>
+
+            <div class="tab-content">
+                <div
+                    class="tab-pane fade show active"
+                    id="pills-login"
+                    role="tabpanel"
+                    aria-labelledby="tab-login">
+                    <form>
+                        
+
+
+                        <div data-mdb-input-init class="form-outline mb-4">
+                            <label class="form-label" for="loginName">Adresse couriel</label>
+                            <input type="email" id="loginName" class="form-control" />
+                        </div>
+
+
+                        <div data-mdb-input-init class="form-outline mb-4">
+                            <label class="form-label" for="loginPassword">Password</label>
+                            <input type="password" id="loginPassword" class="form-control" />
+                        </div>
+
+
+                        <div class="row mb-4">
+                            <div class="col-md-6 d-flex justify-content-center">
+
+                                <div class="form-check mb-3 mb-md-0">
+                                    <input
+                                        class="form-check-input"
+                                        type="checkbox"
+                                        value=""
+                                        id="loginCheck"
+                                        checked
+                                    />
+                                    <label class="form-check-label" for="loginCheck"> Remember me </label>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6 d-flex justify-content-center">
+
+                                <a href="#!">Mot de passe oublier?</a>
+                            </div>
+                        </div>
+
+
+                        <button type="submit" class="btn btn-primary btn-block mb-4">Connexion</button>
+
+
+                        <div class="text-center">
+                            <p>Pas encore membre? Inscrivez vous en tant que <a href="/Coach">coach </a> ou <a href="/Client">client</a></p>
+                        </div>
+                    </form>
+                </div>
+                <div
+                    class="tab-pane fade"
+                    id="pills-register"
+                    role="tabpanel"
+                    aria-labelledby="tab-register"
+                >
+                    <form>
+                        <div class="text-center mb-3">
+                            <p>Sign up with:</p>
+                            <button data-mdb-ripple-init type="button" class="btn btn-secondary btn-floating mx-1">
+                                <i class="fab fa-facebook-f"></i>
+                            </button>
+
+                            <button data-mdb-ripple-init type="button" class="btn btn-secondary btn-floating mx-1">
+                                <i class="fab fa-google"></i>
+                            </button>
+
+                            <button data-mdb-ripple-init type="button" class="btn btn-secondary btn-floating mx-1">
+                                <i class="fab fa-twitter"></i>
+                            </button>
+
+                            <button data-mdb-ripple-init type="button" class="btn btn-secondary btn-floating mx-1">
+                                <i class="fab fa-github"></i>
+                            </button>
+                        </div>
+
+                        <p class="text-center">or:</p>
+
+
+                        <div data-mdb-input-init class="form-outline mb-4">
+                            <input type="text" id="registerName" class="form-control" />
+                            <label class="form-label" for="registerName">Name</label>
+                        </div>
+
+
+                        <div data-mdb-input-init class="form-outline mb-4">
+                            <input type="text" id="registerUsername" class="form-control" />
+                            <label class="form-label" for="registerUsername">Username</label>
+                        </div>
+
+
+                        <div data-mdb-input-init class="form-outline mb-4">
+                            <input type="email" id="registerEmail" class="form-control" />
+                            <label class="form-label" for="registerEmail">Email</label>
+                        </div>
+
+
+                        <div data-mdb-input-init class="form-outline mb-4">
+                            <input type="password" id="registerPassword" class="form-control" />
+                            <label class="form-label" for="registerPassword">Password</label>
+                        </div>
+
+
+                        <div data-mdb-input-init class="form-outline mb-4">
+                            <input type="password" id="registerRepeatPassword" class="form-control" />
+                            <label class="form-label" for="registerRepeatPassword">Repeat password</label>
+                        </div>
+
+
+                        <div class="form-check d-flex justify-content-center mb-4">
+                            <input
+                                class="form-check-input me-2"
+                                type="checkbox"
+                                value=""
+                                id="registerCheck"
+                                checked
+                                aria-describedby="registerCheckHelpText"
+                            />
+                            <label class="form-check-label" for="registerCheck">
+                                I have read and agree to the terms
+                            </label>
+                        </div>
+
+
+                        <button data-mdb-ripple-init type="submit" class="btn btn-primary btn-block mb-3">Sign in</button>
+                    </form>
+                </div>
+            </div>
         </div>
+
     );
 }
 
