051ee844b95fa3cd0f318374fcfb47d78dd6e372
ChadiEC <1985081@collegemv.qc.ca>
2025-03-17 15:04:02 -0400

    Changement du formulaire client

--- src/pages/Client.jsx
+++ src/pages/Client.jsx
@@ -1,5 +1,5 @@
 import 'react';
-import {useState} from "react";
+import React, {useState} from "react";
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
 
@@ -7,7 +7,10 @@ function Client() {
     const [client, setClient] = useState({
         fname:"",
         lname:"",
-        email:""
+        email:"",
+        address:"",
+        phone:"",
+        postalCode:""
     });
 
     const setAttribute = (e) => {
@@ -30,90 +33,129 @@ function Client() {
 
     return (
         <div className="container mt-5">
+
             <div className="row justify-content-lg-start">
+
                 <div className="col-md-6"><h2 className="mb-4">User Information</h2>
+
                     <form className="form-detail" onSubmit={(e) => submitNewClient(e)} method="post">
+
                         <div className="mb-3 "><label htmlFor="lastname" className="form-label text-start d-block">Last
                             Name</label> <input type="text" className="form-control" name="lname" id="lastname"
-                                                placeholder="Enter your last name" required
+                                                placeholder="Entrer votre nom"
                                                 onChange={(e) => setAttribute(e)}
-                                                value={client.lname}/></div>
+                                                value={client.lname}/>
+                        </div>
+
                         <div className="mb-3"><label htmlFor="firstname" className="form-label text-start d-block">First
                             Name</label> <input type="text" className="form-control" name="fname" id="firstname"
-                                                placeholder="Enter your first name" required
+                                                placeholder="Entrer votre prénom"
                                                 onChange={(e) => setAttribute(e)}
-                                                value={client.fname}/></div>
+                                                value={client.fname}/>
+                        </div>
+
                         <div className="mb-3"><label htmlFor="email"
                                                      className="form-label text-start d-block">Email</label> <input
-                            type="email" className="form-control" name="email" id="email" placeholder="Enter your email"
+                            type="email" className="form-control" name="email" id="email"
+                            placeholder="Entrer votre email"
                             required pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}" onChange={(e) => setAttribute(e)}
-                            value={client.email}/></div>
+                            value={client.email}/>
+                        </div>
+
+                        <div className="mb-3"><label htmlFor="address" className="form-label text-start d-block">
+                            Adresse
+                        </label> <input type="text" className="form-control" name="address" id="address"
+                                                   placeholder="Entrez votre adresse"
+                                                   onChange={(e) => setAttribute(e)}
+                                                   value={client.address}/>
+                        </div>
+
+                        <div className="mb-3"><label htmlFor="phone" className="form-label text-start d-block">
+                            Numéro de téléphone
+                        </label> <input type="text" className="form-control" name="phone" id="phone"
+                                        placeholder="Entrez votre numéro de téléphone"
+                                        //required pattern= "\(\d{3}\)\s*\d{3}-\d{4}"
+                                        onChange={(e) => setAttribute(e)}
+                                        value={client.phone}/>
+                        </div>
+
+                        <div className="mb-3"><label htmlFor="codePostale" className="form-label text-start d-block">Code postale
+                        </label> <input type="text" className="form-control" name="postalCode" id="postalCode"
+                                        placeholder="Entrez votre code postale"
+                                        required
+                                        onChange={(e) => setAttribute(e)}
+                                        value={client.postalCode}/>
+                        </div>
+
+
                         <button type="submit" className="btn btn-primary">Submit</button>
+
                     </form>
+
                 </div>
             </div>
         </div>
     );
-       /*
-       return(
-       < div >
-        < form >
-            <h3>Incription en tant que client</h3><br/>
-            <div class="row mb-4">
-                <div class="col">
-                    <div data-mdb-input-init class="form-outline">
-                        <label class="form-label" for="form6Example1">Nom</label>
-                        <input type="text" id="form6Example1" class="form-control" />
-                    </div>
-                </div>
-                <div class="col">
-                    <div data-mdb-input-init class="form-outline">
-                        <label class="form-label" for="form6Example2">Prénom</label>
-                        <input type="text" id="form6Example2" class="form-control" />
-                    </div>
-                </div>
-            </div>
+    /*
+    return(
+    < div >
+     < form >
+         <h3>Incription en tant que client</h3><br/>
+         <div class="row mb-4">
+             <div class="col">
+                 <div data-mdb-input-init class="form-outline">
+                     <label class="form-label" for="form6Example1">Nom</label>
+                     <input type="text" id="form6Example1" class="form-control" />
+                 </div>
+             </div>
+             <div class="col">
+                 <div data-mdb-input-init class="form-outline">
+                     <label class="form-label" for="form6Example2">Prénom</label>
+                     <input type="text" id="form6Example2" class="form-control" />
+                 </div>
+             </div>
+         </div>
 
 
-            <div data-mdb-input-init class="form-outline mb-4">
-                <label class="form-label" for="form6Example3">Adresse couriel</label>
-                <input type="text" id="form6Example3" class="form-control" />
+         <div data-mdb-input-init class="form-outline mb-4">
+             <label class="form-label" for="form6Example3">Adresse couriel</label>
+             <input type="text" id="form6Example3" class="form-control" />
 
-            </div>
-
-
-            <div data-mdb-input-init class="form-outline mb-4">
-                <label class="form-label" for="form6Example4">Adresse </label>
-                <input type="text" id="form6Example4" class="form-control" />
-                
-            </div>
+         </div>
 
 
-            <div data-mdb-input-init class="form-outline mb-4">
-                <label class="form-label" for="form6Example5">Code postal</label>
-                <input type="email" id="form6Example5" class="form-control" />
-                
-            </div>
+         <div data-mdb-input-init class="form-outline mb-4">
+             <label class="form-label" for="form6Example4">Adresse </label>
+             <input type="text" id="form6Example4" class="form-control" />
+
+         </div>
 
 
-            <div data-mdb-input-init class="form-outline mb-4">
-            <label class="form-label" for="form6Example6">Phone</label>
-                <input type="number" id="form6Example6" class="form-control" />
-                
-            </div>
+         <div data-mdb-input-init class="form-outline mb-4">
+             <label class="form-label" for="form6Example5">Code postal</label>
+             <input type="email" id="form6Example5" class="form-control" />
+
+         </div>
 
 
-            <div data-mdb-input-init class="form-outline mb-4">
-            <label class="form-label" for="form6Example7">Information additionnel</label>
-                <textarea class="form-control" id="form6Example7" rows="4"></textarea>
-                
-            </div>
+         <div data-mdb-input-init class="form-outline mb-4">
+         <label class="form-label" for="form6Example6">Phone</label>
+             <input type="number" id="form6Example6" class="form-control" />
+
+         </div>
 
 
-            <button data-mdb-ripple-init type="button" class="btn btn-primary btn-block mb-4">Envoyer</button>
-        </form >
-    </div >
-    );*/
+         <div data-mdb-input-init class="form-outline mb-4">
+         <label class="form-label" for="form6Example7">Information additionnel</label>
+             <textarea class="form-control" id="form6Example7" rows="4"></textarea>
+
+         </div>
+
+
+         <button data-mdb-ripple-init type="button" class="btn btn-primary btn-block mb-4">Envoyer</button>
+     </form >
+ </div >
+ );*/
 }
 
 export default Client;
